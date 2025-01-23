470b1b1388cc83b249f5f0e74e9f8061c59e2896
yoshii mika <mika.1120.radwimps@icloud.com>
2025-01-23 13:24:56 +0900

    予約時バリデーションルールの修正

--- src/app/Http/Requests/ReservationRequest.php
+++ src/app/Http/Requests/ReservationRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ReservationRequest extends FormRequest
 {
@@ -25,8 +27,13 @@ class ReservationRequest extends FormRequest
     {
         return [
             'shop_id' => ['required', 'exists:shops,id'],
-            'date' => ['required', 'date'],
-            'time' => ['required', 'date_format:H:i'],
+            'date' => ['required', 'date', 'after_or_equal:' . Carbon::now()->toDateString()],
+            'time' => ['required', 'date_format:H:i', function ($attribute, $value, $fail) {
+                $reservationDateTime = Carbon::parse($this->date . ' ' . $value);
+                if ($reservationDateTime->isBefore(Carbon::now())) {
+                    $fail('現在の日時より後の時間を指定してください。');
+                }
+            }],
             'number' => ['required', 'integer', 'min:1'],
         ];
     }
@@ -38,6 +45,7 @@ class ReservationRequest extends FormRequest
             'shop_id.required' => '店舗を選択してください。',
             'shop_id.exists' => '選択された店舗は存在しません。',
             'date.required' => '予約日を選択してください。',
+            'date.after_or_equal' => '今日以降の日付を指定してください。',
             'time.required' => '予約時間を選択してください。',
             'number.required' => '人数を入力してください。',
         ];
